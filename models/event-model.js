9a47d4a41d2159d046bc4a1e0be5b163829ce649
Varshil-Shah <varshilashah09@gmail.com>
2022-10-03 17:01:01 +0530

    Add more validation in event-model.js file

--- models/event-model.js
+++ models/event-model.js
@@ -2,10 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    eventID : {type: String, unique: true, required: true },
-    title: { type: String, unique: true, required: true },
-    description: { type: String, required: true },
-    date: { type: Date, required: true },
+    eventID : {
+        type: String,
+        unique: true, 
+        required: [true, 'Please provide an event Id']
+    },
+    title: { 
+        type: String, 
+        unique: true, 
+        required: [true, 'Please provide a title for an event'],
+        trim: true,
+        validate: {
+            validator: function (value) {
+                return /^[a-zA-Z ]*$/.test(value);
+            },
+            message: 'An event title should contain only alphabets',
+        },
+    },
+    description: { 
+        type: String,
+        trim: true,
+        required: [true, 'Please provide a description for an event'],
+    },
+    date: { type: Date, required: [true, 'Please provide date for an event'] },
     createdDate: { type: Date, default: Date.now }
 });
 
